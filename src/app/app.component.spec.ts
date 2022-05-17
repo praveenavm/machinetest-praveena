eb58ed2b03b1dd6bf2a53e45f9eca30d4a2da1f2
praveenavm <praveena.vm@faithinfotech.in>
2022-05-17 16:04:58 +0530

    pushing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scholarship'`, () => {
+  it(`should have as title 'ScholarshipManagement'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('scholarship');
+    expect(app.title).toEqual('ScholarshipManagement');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('scholarship app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ScholarshipManagement app is running!');
   });
 });
